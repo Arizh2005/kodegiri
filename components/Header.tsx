454a78895f751c56a7e3c8432659078f762c9704
Riz <123041516+Arizh2005@users.noreply.github.com>
2025-05-25 17:46:00 +0700

    Navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Container from './Container';
 import Logo from './Logo';
+import HeaderMenu from './HeaderMenu';
 
 const Header = () => {
     return (
-        <header>
-            <Container>
-                <Logo/>
+        <header className="bg-white py-5 border-b border-b-black/20">
+            <Container className="flex items-center justify-between">
+                <Logo />
+                <HeaderMenu />
+                <div>other</div>
             </Container>
         </header>
     );
---
+++ constants/data.ts
@@ -0,0 +1,9 @@
+import { title } from "process";
+
+export const headerData = [
+    {title: "Home", href: "/"},
+    {title: "Shop", href: "/shop"},
+    {title: "blog", href: "/blog"},
+    {title: "Contact", href: "/contact"},
+    {title: "Hot Deal", href: "/deal"},
+]
