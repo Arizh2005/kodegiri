2f6d315863eb2534fd2913a554c3682cac635376
Riz <123041516+Arizh2005@users.noreply.github.com>
2025-05-29 19:25:58 +0700

    (Update) Sidebar

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -13,10 +13,11 @@ interface SidebarProps {
 
 const SideMenu: FC<SidebarProps> = ({isOpen,onClose}) => {
   const pathname = usePathname();
-  const SidebarRef=useOutsideClick<HTMLDivElement>('onClose');
+  const SidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
     <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"} hoverEffect`}>
-      <div className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-kodegiri-dark-green flex flex-col gap-6">
+      <div ref={SidebarRef}
+      className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-kodegiri-dark-green flex flex-col gap-6">
         <div className="flex items-center justify-between gap-5">
           <Logo className="text-white" 
             spanDesign="group-hover:text-white"/>
